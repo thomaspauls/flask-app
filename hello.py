f367d643ebfd8e7c75209c876d6b6a39a9be1799
hshar94 <hemantsharma@Hemants-MacBook-Air.local>
2021-05-07 07:42:26 +0530

    fix

--- hello.py
+++ hello.py
@@ -18,7 +18,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Welcome to Intellipaat Demo</h1><p>This site is a prototype API for moving data between sources. Use /migrate to start the job.</p>"
 
 
 @app.route('/migrate', methods=['GET'])
